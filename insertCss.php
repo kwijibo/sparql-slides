4a4a8871a7cf03659fd48bf11fcf3249c589a49f
Keith Alexander <K.J.W.Alexander@gmail.com>
2010-12-07 08:06:49 +0000

    added html + css

---
+++ insertCss.php
@@ -0,0 +1,6 @@
+<?php
+$html = file_get_contents('slides.html');
+//$html = preg_replace('@<script.+?</script>@ms', '', $html);
+$html = preg_replace('@<style type="text/css" media="screen">.+?</style>@msi', '<link rel="stylesheet" href="slides.css"/><script type="text/javascript" src="slides.js"></script>', $html);
+file_put_contents('slides.html', $html);
+?>
