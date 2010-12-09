78aebc99765d93c55eabccc9d86b5d7e9ecd00f2
Keith Alexander <K.J.W.Alexander@gmail.com>
2010-12-09 07:33:33 +0000

    edited slides

--- insertCss.php
+++ insertCss.php
@@ -2,8 +2,6 @@
 $html = file_get_contents('slides.html');
 //$html = preg_replace('@<script.+</script>@ms', '', $html);
 $html = preg_replace('@<style type="text/css" media="screen">.+?</style>@msi', '<link rel="stylesheet" href="slides.css"/><script type="text/javascript" src="slides.js"></script>', $html);
-
-
-// $html = str_replace('</body>', '<script type="text/javascript" src="slides.js"></script></body>', $html);
+ $html = str_replace('$(document).bind(\'click\', clickMove);', '', $html);
 file_put_contents('slides.html', $html);
 ?>
