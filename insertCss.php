c59179aab92fb26f2a740e079f9d79b78cb9e61e
Keith Alexander <K.J.W.Alexander@gmail.com>
2010-12-14 09:00:39 +0000

    edited typos and fixed example answer

--- insertCss.php
+++ insertCss.php
@@ -3,5 +3,7 @@ $html = file_get_contents('slides.html');
 //$html = preg_replace('@<script.+</script>@ms', '', $html);
 $html = preg_replace('@<style type="text/css" media="screen">.+?</style>@msi', '<link rel="stylesheet" href="slides.css"/><script type="text/javascript" src="slides.js"></script>', $html);
  $html = str_replace('$(document).bind(\'click\', clickMove);', '', $html);
+
+$html = str_replace('<title>Slides</title>', '<title>A Day of SPARQL: Slides</title>', $html);
 file_put_contents('slides.html', $html);
 ?>
